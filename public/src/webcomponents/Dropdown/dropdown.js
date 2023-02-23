758f7e1dfb7975591afdfcaab81f4328df9f402b
Fannese <manuelafannese51@yahoo.com>
2023-02-23 15:02:50 +0100

    make selected Item show Detail and delete Bootrap

--- public/src/webcomponents/Dropdown/dropdown.js
+++ public/src/webcomponents/Dropdown/dropdown.js
@@ -6,47 +6,69 @@ class Dropdown extends HTMLElement{
        this.shadowRoot.innerHTML=`
 <style>
 .dropdown{
-position: relative;
-display: inline-block;
+    position: relative;
+    display: inline-block;
 }
 .dropdown-toggle{
-border: none;
-background-color: lightseagreen;
-color: #333;
-cursor: pointer;
-padding: 10px;
-font-size: 16px;
+    border: none;
+    background-color: lightseagreen;
+    color: #333;
+    cursor: pointer;
+    padding: 10px;
+    font-size: 16px;
+    transition: 1.5s;
+   left: 50%;
+   top: 50%;
+   transform: translate(70%, 50%);
 }
 .dropdown-toggle:hover{
-background-color: #eee;
+    background-color: #eee;
 }
 .dropdown-menü{
-position: absolute;
-z-index: 1;
-background-color: #fff;
-box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-border-radius: 5px;
-padding:10px;
-display: none;
+    position: absolute;
+    z-index: 1;
+    background-color: #fff;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    border-radius: 5px;
+    padding:10px;
+    display: none;
+  
 }
 .dropdown-menü a{
-display: block;
-padding: 10px;
-text-decoration: none;
-color: #333;
+    display: block;
+    padding: 10px;
+    text-decoration: none;
+    color: #333;
 }
 .dropdown-menü a:hover{
-background-color: #eee;
+    background-color: #eee;
 }
 .dropdown:hover .dropdown-menü{
-display: block;
+    display: block;
 }
 ::slotted(p){
-border: 2px;
-text-align: center;
-font-size: 1.5rem;
-font-family: sans-serif;
-padding: 2px;
+    border: 2px;
+    text-align: center;
+    font-size: 1.5rem;
+    font-family: sans-serif;
+    padding: 2px;
+    justify-content: center;
+   
+}
+.details{
+justify-content: space-between;
+flex-direction: column;
+align-items: flex-start;
+display: flex;
+}
+.details img{
+width: 280px;
+height: 300px;
+
+}
+..details p{
+align-items: flex-end;
+display: flex;
 }
 </style>
 <slot></slot>
@@ -57,7 +79,16 @@ padding: 2px;
   <ul class="dropdown-menü">
   </ul>
 </div>
+<div class="details">
+<h2></h2>
+<p></p>
+
+</div>
        `;
+       this.details= this.shadowRoot.querySelector('.details');
+       this.filmTitle=this.shadowRoot.querySelector('.details h2');
+   //   filmImage=this.shadowRoot.querySelector('.details img');
+       this.detailDescription=this.shadowRoot.querySelector('.details p');
    }
    connectedCallback(){
        //meine Elemente mit der DOM verbinden und freigeben
@@ -72,14 +103,28 @@ this.loadData();
      this.render(resp.items);
 
    }
+
    render(items) {
        const menu = this.shadowRoot.querySelector('.dropdown-menü');
       items.forEach((item)=>{
            const link = document.createElement('a');
-           link.setAttribute('href','#');
+           link.setAttribute('href',"#");
            link.textContent=item.name;
+           link.addEventListener('click',()=>{
+               this.filmImage =document.createElement('img');
+               this.shadowRoot.querySelector('.details').appendChild(this.filmImage);
+               this.showDetails(item);
+           });
            menu.appendChild(link);
        });
    }
+   //Detail Anschicht
+   showDetails(item){
+       this.filmTitle.textContent=item.title;
+    this.filmImage.src=item.image;
+       this.detailDescription.textContent=item.description;
+   }
+    disconnectedCallback(){
+    }
 }
 customElements.define('ba-dropdown',Dropdown);
--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 const PORT = process.env.PORT = 4000;
 
 //Laden der JSON-Datei mit dem Dropdown-Menü.
-const menuItems = require("../webcomponents/Dropdown/service.json")
+/*const menuItems = require("../BA_Webkomponent/public/src/webcomponents/Dropdown/service.json");
+//der JSON-datei wird bereitgestellt
 app.get('/items', (req, res)=>{
    res.send(menuItems);
-});
+});*/
 
-// The magic happens here
 app.use(express.static(path.join(__dirname +'/public/src')));
 
 app.listen(PORT, () => {
